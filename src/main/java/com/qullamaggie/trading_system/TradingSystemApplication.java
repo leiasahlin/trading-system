a48df06789d1667c635bdf60d425c30b735c1147
leia <c25lhn@cs.umu.se>
2026-06-11 14:39:25 +0200

    Init project structure

---
+++ src/main/java/com/qullamaggie/trading_system/TradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.qullamaggie.trading_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingSystemApplication.class, args);
+	}
+
+}
