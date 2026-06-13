4f44970c166a9653c2dcf7bac6a8f200abcc94db
leia <c25lhn@cs.umu.se>
2026-06-13 11:32:12 +0200

    Add logic to Alert entity

--- src/main/java/com/qullamaggie/tradingsystem/data/entity/Alert.java
+++ src/main/java/com/qullamaggie/tradingsystem/data/entity/Alert.java
@@ -1,4 +1,35 @@
 package com.qullamaggie.tradingsystem.data.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Table(name = "alerts")
 public class Alert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false, length = 30)
+    private String type;
+
+    @Column(precision = 12, scale = 4)
+    private BigDecimal price;
+
+    private boolean seen = false;
+
+    @Column(columnDefinition = "TEXT")
+    private String message;
 }
