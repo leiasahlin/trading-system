5a859aca8b4a998db87b1ae0f1b1198eab93258d
leia <c25lhn@cs.umu.se>
2026-07-21 20:57:11 +0200

    Correct time intervall

--- src/main/java/com/qullamaggie/tradingsystem/data/provider/TwelveDataProvider.java
+++ src/main/java/com/qullamaggie/tradingsystem/data/provider/TwelveDataProvider.java
@@ -79,7 +79,7 @@ public class TwelveDataProvider implements MarketDataProvider {
                         .path("/time_series")
                         .queryParam("symbol", symbol)
                         .queryParam("interval", "1min")
-                        .queryParam("outputsize", 390)
+                        .queryParam("outputsize", 30)
                         .queryParam("timezone", "America/New_York")
                         .queryParam("apikey", apiKey)
                         .build())
